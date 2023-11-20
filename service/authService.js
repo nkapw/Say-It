f39515f42a6f85ce88d756e7aba97867d5896734
Imam Ahmad Fahrezi <imamfahrezi24@gmail.com>
2023-11-20 12:45:37 +0700

    add name column di register handler

--- service/authService.js
+++ service/authService.js
@@ -8,32 +8,33 @@ const {client} = require('../connection/db')
  
  const registerService =  async (request, h) => {
             try {
-                const { username, email, password } = request.payload;
+                const { username, email, password, name } = request.payload;
     
                 // Validasi input
                 const schema = Joi.object({
                     username: Joi.string().alphanum().min(3).max(30).required(),
                     email: Joi.string().email().required(),
                     password: Joi.string().min(6).required(),
+                    name: Joi.string().required()
                 });
     
-                const validationResult = schema.validate({ username, email, password });
+                const validationResult = schema.validate({ username, email, password, name });
                 if (validationResult.error) {
-                    return h.response(validationResult.error.details[0].message).code(400);
+                    return h.response({'message':validationResult.error.details[0].message}).code(400);
                 }
     
                 // Hash password sebelum disimpan
                 const hashedPassword = await bcrypt.hash(password, 10);
     
                 // Simpan data registrasi ke database
-                const query = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)';
-                const values = [username, email, hashedPassword];
+                const query = 'INSERT INTO users (username, email, password, name) VALUES ($1, $2, $3, $4)';
+                const values = [username, email, hashedPassword, name];
                 await client.query(query, values);
     
-                return h.response('Registrasi berhasil').code(201);
+                return h.response({"message":'Registrasi berhasil', 'email': email, 'name': name}).code(201);
             } catch (error) {
                 console.error(error);
-                return h.response('Terjadi kesalahan internal').code(500);
+                return h.response({"message":'Terjadi kesalahan internal'}).code(500);
             }
         }
 
@@ -57,13 +58,13 @@ const loginService = async (request, h) => {
                 const result = await client.query(query, [username]);
     
                 if (result.rows.length === 0) {
-                    return h.response('Login gagal. Pengguna tidak ditemukan.').code(401);
+                    return h.response({'message':'Login gagal. Pengguna tidak ditemukan.'}).code(401);
                 }
     
                 // Verifikasi password
                 const isValidPassword = await bcrypt.compare(password, result.rows[0].password);
                 if (!isValidPassword) {
-                    return h.response('Login gagal. Password salah.').code(401);
+                    return h.response({'message':'Login gagal. Password salah.'}).code(401);
                 }
     
                 const secret_key = randomUUID.toString();
@@ -75,7 +76,7 @@ const loginService = async (request, h) => {
                 return h.response({ token }).code(200);
             } catch (error) {
                 console.error(error);
-                return h.response('Terjadi kesalahan internal').code(500);
+                return h.response({'message':'Terjadi kesalahan internal'}).code(500);
             }
         
     }
